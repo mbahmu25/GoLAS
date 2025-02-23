c15e3fcd657a5594b309c3b6844766ebb9798bb2
mbahmu25 <nikogenta25@gmail.com>
2025-02-24 00:13:22 +0700

    first push

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"GoLas/readLas"
 	"fmt"
+	"os"
 )
 
 func main() {
+	filePath := os.Args
+	fmt.Println(filePath)
 	fileData := readLas.Read_file("output_fix.las")
 	fmt.Println(fileData[102420])
 }
